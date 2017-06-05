199d0d504a5921c4d656d4e0947c9f8b2ef05764
Nikka-Sher <npiterman@gmail.com>
2017-06-05 16:43:21 -0700

    added second, details view controller

---
+++ Pokedex3/PokemonDetailVC.swift
@@ -0,0 +1,40 @@
+//
+//  PokemonDetailVC.swift
+//  Pokedex3
+//
+//  Created by Konstantine Piterman on 6/1/17.
+//  Copyright © 2017 Konstantine Piterman. All rights reserved.
+//
+
+import UIKit
+
+class PokemonDetailVC: UIViewController {
+
+    var pokemon: Pokemon!
+    
+    @IBOutlet weak var nameLbl: UILabel!
+ 
+    @IBOutlet weak var mainImg: UIImageView!
+
+    @IBOutlet weak var descriptionLbl: UILabel!
+    @IBOutlet weak var typeLbl: UILabel!
+    @IBOutlet weak var defenseLbl: UILabel!
+    @IBOutlet weak var heightLbl: UILabel!
+    @IBOutlet weak var weightLbl: UILabel!
+    @IBOutlet weak var attackLbl: UILabel!
+    @IBOutlet weak var nextEvoImg: UIImageView!
+    @IBOutlet weak var evoLbl: UILabel!
+    @IBOutlet weak var currentEvoImg: UIImageView!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        nameLbl.text = pokemon.name.capitalized
+        
+
+        // Do any additional setup after loading the view.
+    }
+
+
+    
+    
+}
